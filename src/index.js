0a18bdb723ba6d87811131aecce8d5f998f4ced1
Benjamin Clot <benjamin.clot@gmail.com>
2018-08-27 14:39:42 +0200

    Updated response headers

--- src/index.js
+++ src/index.js
@@ -94,6 +94,10 @@ if (cluster.isMaster) {
       res.set('Timing-Allow-Origin', allowOrigin);
     }
 
+    res.set('Content-Type', 'text/plain');
+    res.set('Cache-control', 'no-store, no-cache, private');
+    res.set('Pragma', 'no-cache');
+
     insertData(req.body)
       .then(() => {
         res.status(200).end('OK');
